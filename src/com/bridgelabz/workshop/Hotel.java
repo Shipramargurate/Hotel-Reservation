8eec1b55311d3e4b3b4b4e815a769ae3780786e8
shipramargurate <shivanikumari22322@gmail.com>
2022-11-03 11:44:43 +0530

    [Shivani]: Welcome to reservation program

---
+++ src/com/bridgelabz/workshop/Hotel.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.workshop;
+
+public class Hotel {
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO HOTEL RESERVATION PROGRAM IN HOTELRESERVATION");
+	}
+
+}
